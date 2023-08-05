f69240ebea5a57e097056bec560cc6a200653c2c
David Binder <david.binder@uni-tuebingen.de>
2023-08-05 18:21:15 +0200

    more...

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # tasty-test
 Try out some stuff with tasty
+
+```console
+> cabal run tasty-test-test -- --help
+Mmm... tasty test suite
+
+Usage: tasty-test-test [-p|--pattern PATTERN] [-t|--timeout DURATION] 
+                       [-c|--report-coverage]
+
+Available options:
+  -h,--help                Show this help text
+  -p,--pattern PATTERN     Select only tests which satisfy a pattern or awk
+                           expression
+  -t,--timeout DURATION    Timeout for individual tests (suffixes: ms,s,m,h;
+                           default: s)
+  -c,--report-coverage     Generate per-test coverage data
+
+> cabal run tasty-test-test -- -c
+...
+```
--- src/Test/Tasty/CoverageReporter.hs
+++ src/Test/Tasty/CoverageReporter.hs
@@ -1,14 +1,44 @@
+{-# LANGUAGE InstanceSigs #-}
 module Test.Tasty.CoverageReporter (coverageReporter) where
 
 import Test.Tasty
-import Test.Tasty.Ingredients (Ingredient(..))
-import Test.Tasty.Options (OptionDescription, OptionSet)
+import Test.Tasty.Ingredients
+import Test.Tasty.Options
+import Test.Tasty.Runners
+import Data.Typeable
+import Options.Applicative
+
+newtype ReportCoverage = MkReportCoverage   Bool
+  deriving (Eq, Ord, Typeable)
+
+instance IsOption ReportCoverage where
+    defaultValue :: ReportCoverage
+    defaultValue  = MkReportCoverage False
+    parseValue = fmap MkReportCoverage . safeReadBool
+    optionName = pure "report-coverage"
+    optionHelp = pure "Generate per-test coverage data"
+    optionCLParser = mkFlagCLParser (short 'c') (MkReportCoverage True)
+
+
+
+-- | Obtain the list of all tests in the suite
+testNames :: OptionSet -> TestTree -> [TestName]
+testNames {- opts -} {- tree -} =
+  foldTestTree
+    trivialFold
+      { foldSingle = \_opts name _test -> [name]
+      , foldGroup = \_opts groupName names -> map ((groupName ++ ".") ++) names
+      }
 
 coverageReporter :: Ingredient
 coverageReporter = TestManager coverageOptions coverageRunner
 
 coverageOptions :: [OptionDescription]
-coverageOptions = []
+coverageOptions = [Option (Proxy :: Proxy ReportCoverage)]
 
 coverageRunner :: OptionSet -> TestTree -> Maybe (IO Bool)
-coverageRunner _ _ = Nothing
+coverageRunner opts tree = case lookupOption opts of
+  MkReportCoverage False -> Nothing
+  MkReportCoverage True -> Just $ do
+    mapM_ putStrLn $ testNames opts tree
+    return True
