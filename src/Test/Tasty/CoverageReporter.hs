4dba60c0f4cd057aacf354bd65854df28737e09c
David Binder <david.binder@uni-tuebingen.de>
2023-09-10 17:36:23 +0200

    Format code with fourmolu and add fourmolu.yaml config file

--- src/Test/Tasty/CoverageReporter.hs
+++ src/Test/Tasty/CoverageReporter.hs
@@ -1,30 +1,33 @@
-{-# LANGUAGE InstanceSigs, NamedFieldPuns, CPP #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Test.Tasty.CoverageReporter (coverageReporter) where
 
+import Control.Monad (forM_)
+import Data.Bifunctor (first)
+import Data.Foldable (fold)
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NE
+import Data.Typeable
+import System.FilePath ((<.>), (</>))
 import Test.Tasty
 import Test.Tasty.Ingredients
 import Test.Tasty.Options
-import Test.Tasty.Runners
 import Test.Tasty.Providers
-import Data.Typeable
-import Trace.Hpc.Reflect ( clearTix, examineTix )
-import Trace.Hpc.Tix ( writeTix, Tix(..), TixModule(..) )
-import System.FilePath ( (<.>), (</>) )
-import Control.Monad (forM_)
-import qualified Data.List.NonEmpty as NE
-import Data.List.NonEmpty ( NonEmpty )
-import Data.Foldable (fold)
-import Data.Bifunctor (first)
+import Test.Tasty.Runners
+import Trace.Hpc.Reflect (clearTix, examineTix)
+import Trace.Hpc.Tix (Tix (..), TixModule (..), writeTix)
 
 newtype ReportCoverage = MkReportCoverage Bool
   deriving (Eq, Ord, Typeable)
 
 instance IsOption ReportCoverage where
-    defaultValue  = MkReportCoverage False
-    parseValue = fmap MkReportCoverage . safeReadBool
-    optionName = pure "report-coverage"
-    optionHelp = pure "Generate per-test coverage data"
-    optionCLParser = mkFlagCLParser mempty (MkReportCoverage True)
+  defaultValue = MkReportCoverage False
+  parseValue = fmap MkReportCoverage . safeReadBool
+  optionName = pure "report-coverage"
+  optionHelp = pure "Generate per-test coverage data"
+  optionCLParser = mkFlagCLParser mempty (MkReportCoverage True)
 
 newtype RemoveTixHash = MkRemoveTixHash Bool
   deriving (Eq, Ord, Typeable)
@@ -37,17 +40,17 @@ instance IsOption RemoveTixHash where
   optionCLParser = mkFlagCLParser mempty (MkRemoveTixHash True)
 
 coverageOptions :: [OptionDescription]
-coverageOptions = [ Option (Proxy :: Proxy ReportCoverage)
-                  , Option (Proxy :: Proxy RemoveTixHash)
-                  ]
-
+coverageOptions =
+  [ Option (Proxy :: Proxy ReportCoverage),
+    Option (Proxy :: Proxy RemoveTixHash)
+  ]
 
 tixDir :: FilePath
 tixDir = "tix"
 
 -- | Obtain the list of all tests in the suite
 testNames :: OptionSet -> TestTree -> IO ()
-testNames  os tree = forM_ (foldTestTree coverageFold os tree) $ \(s,f) -> f (fold (NE.intersperse "." s))
+testNames os tree = forM_ (foldTestTree coverageFold os tree) $ \(s, f) -> f (fold (NE.intersperse "." s))
 
 type FoldResult = [(NonEmpty TestName, String -> IO ())]
 
@@ -59,26 +62,26 @@ groupFold :: OptionSet -> TestName -> FoldResult -> FoldResult
 groupFold _ groupName acc = fmap (first (NE.cons groupName)) acc
 #endif
 
-
 -- | Collect all tests and
 coverageFold :: TreeFold FoldResult
-coverageFold = trivialFold
-       { foldSingle = \opts name test -> do
-          let f n = do
-                -- Collect the coverage data for exactly this test.
-                clearTix
-                result <- run opts test (\_ -> pure ())
-                tix <- examineTix
-                let filepath = tixFilePath n result
-                writeTix filepath (removeHash opts tix)
-                putStrLn ("Wrote coverage file: " <> filepath)
-          pure (NE.singleton name, f),
-          -- Append the name of the testgroup to the list of TestNames
-          foldGroup = groupFold
-        }
+coverageFold =
+  trivialFold
+    { foldSingle = \opts name test -> do
+        let f n = do
+              -- Collect the coverage data for exactly this test.
+              clearTix
+              result <- run opts test (\_ -> pure ())
+              tix <- examineTix
+              let filepath = tixFilePath n result
+              writeTix filepath (removeHash opts tix)
+              putStrLn ("Wrote coverage file: " <> filepath)
+        pure (NE.singleton name, f),
+      -- Append the name of the testgroup to the list of TestNames
+      foldGroup = groupFold
+    }
 
 tixFilePath :: TestName -> Result -> FilePath
-tixFilePath tn Result { resultOutcome }  =
+tixFilePath tn Result {resultOutcome} =
   tixDir </> generateValidFilepath tn <.> outcomeSuffix resultOutcome <.> ".tix"
 
 -- | We want to compute the file suffix that we use to distinguish
@@ -92,7 +95,6 @@ outcomeSuffix (Failure TestDepFailed) = "SKIPPED"
 
 coverageReporter :: Ingredient
 coverageReporter = TestManager coverageOptions coverageRunner
-
 
 coverageRunner :: OptionSet -> TestTree -> Maybe (IO Bool)
 coverageRunner opts tree = case lookupOption opts of
@@ -110,8 +112,8 @@ generateValidFilepath = filter (`notElem` pathSeparators)
   where
     -- Include both Windows and Posix, so that generated .tix files
     -- are consistent among systems.
-    pathSeparators = ['\\','/']
-  
+    pathSeparators = ['\\', '/']
+
 removeHash :: OptionSet -> Tix -> Tix
 removeHash opts (Tix txs) = case lookupOption opts of
   MkRemoveTixHash False -> Tix txs
@@ -119,4 +121,3 @@ removeHash opts (Tix txs) = case lookupOption opts of
 
 removeHashModule :: TixModule -> TixModule
 removeHashModule (TixModule name _hash i is) = TixModule name 0 i is
-
